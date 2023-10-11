9df7e4dd432f858445a08a3f19fd500d30b967ec
U-HPLAPTOP\runte <418331@bsd48.org>
2023-10-10 19:27:39 -0700

    Finished with comments

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,13 +1,22 @@
+/*
+  Author: Tejas Pandit
+  Last Edited: 10/10/23
+  This program creates a game of two player tic-tac-toe (no AI)
+  The game keeps track of wins, losses, ties, and allows the users to play again
+ */
+
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
+//need this line to allow us to use the checkWin function later
 bool checkWin(char player, char board[4][4]);
 
 int main()
 {
-  char board[4][4];
+  //variables
+  char board[4][4]; //the board will be a 4x4 (actual board and labels)
   bool stillPlaying = true;
   int playerMove = 1;
   int inputColumn;
@@ -19,7 +28,8 @@ int main()
   int numWinsO = 0;
   int numTies = 0;
   char input;
-  
+
+  //framework of the board, a, b, and c represent rows, 1, 2, and 3 represent columns
   board[0][0] = ' ';
   board[1][0] = 'a';
   board[2][0] = 'b';
@@ -27,12 +37,15 @@ int main()
   board[0][1] = '1';
   board[0][2] = '2';
   board[0][3] = '3';
-  
+
+  //initialize the board to be clear 
   for (int i = 1; i < 4; i++){
     for (int j = 1; j < 4; j++){
       board[i][j] = '-';
     }
   }
+
+  //print out empty board and statements telling number of wins/ties
   cout << "X Wins: " << numWinsX << endl;
   cout << "O Wins: " << numWinsO << endl;
   cout << "Ties: " << numTies << endl;
@@ -43,26 +56,35 @@ int main()
     cout << "" << endl;
   }
 
+  //while loops that runs until the users no longer want to kepp playing
   while (stillPlaying == true){
+
+    //ask for the row and column the user wants to play in and save in variables
     cout << "Enter the row you would like to play in (a, b, or c)." << endl;
     cin >> inputRow;
     cout << "Enter the column you would like to play in (1, 2, or 3)." << endl;
     cin >> inputColumn;
 
+    //goes through all possible moves (18 possibles moves as 9 possible for X and 9 possible for O)
     for (int i = 1; i < 4; i++){
+      //checks if the move attempted was made by X in an empty space on the first row
       if(inputRow == 'a' && inputColumn == i && playerMove == 1 && board[1][i] == '-'){
+	//move was legal so change space selected to piece
 	board[1][i] = 'X';
-	moves++;
+	moves++; //a move has been made
+	//check if the move won the game
 	if(checkWin(X, board) == true){
 	  cout << "X Wins!" << endl;
-	  numWinsX++;
+	  numWinsX++; //if the game was won, increase number of wins
 	  stillPlaying = false;
 	}
-	playerMove = 2;
+	playerMove = 2; //change player move
       }
 
+      //checks if the move attempted was made by X in an empty space on the second row
       else if(inputRow == 'b' && inputColumn == i && playerMove == 1 && board[2][i] == '-'){
-        board[2][i] = 'X';
+	//follows same format as the first if statement
+	board[2][i] = 'X';
 	moves++;
         if(checkWin(X, board) == true){
           cout << "X Wins!" << endl;
@@ -72,8 +94,10 @@ int main()
 	playerMove = 2;
       }
 
+      //checks if the move attempted was made by X in an empty space on the third row
       else if(inputRow == 'c' && inputColumn == i && playerMove == 1 && board[3][i] == '-'){
-        board[3][i] = 'X';
+	//follows same format as the first if statement
+	board[3][i] = 'X';
 	moves++;
         if(checkWin(X, board) == true){
           cout << "X Wins!" << endl;
@@ -83,8 +107,10 @@ int main()
 	playerMove = 2;
       }
 
+      //checks if the move attempted was made by O in an empty space on the first row
       else if(inputRow == 'a' && inputColumn == i && playerMove == 2 && board[1][i] == '-'){
-        board[1][i] = 'O';
+	//follows same format as the first if statement
+	board[1][i] = 'O';
 	moves++;
         if(checkWin(O, board) == true){
           cout << "O Wins!" << endl;
@@ -94,8 +120,10 @@ int main()
         playerMove = 1;
       }
 
+      //checks if the move attempted was made by O in an empty space on the second row
       else if(inputRow == 'b' && inputColumn == i && playerMove == 2 && board[2][i] == '-'){
-        board[2][i] = 'O';
+	//follows same format as the first if statement
+	board[2][i] = 'O';
 	moves++;
         if(checkWin(O, board) == true){
           cout << "O Wins!" << endl;
@@ -105,8 +133,10 @@ int main()
         playerMove = 1;
       }
 
+      //checks if the move attempted was made by O in an empty space on the second row
       else if(inputRow == 'c' && inputColumn == i && playerMove == 2 && board[3][i] == '-'){
-        board[3][i] = 'O';
+	//follows same format as the first if statement
+	board[3][i] = 'O';
 	moves++;
         if(checkWin(O, board) == true){
           cout << "O Wins!" << endl;
@@ -121,6 +151,7 @@ int main()
 
     }
 
+    //if the number of moves is 9 and nobody has won, it is a tie
     if(moves >= 9){
       cout << "It is a Tie!" << endl;
       numTies++;
@@ -128,6 +159,7 @@ int main()
     }
 
 
+    //print out the number of wins/ties and the board after each move
     cout << "X Wins: " << numWinsX << endl;
     cout << "O Wins: " << numWinsO << endl;
     cout << "Ties: " << numTies << endl;
@@ -138,12 +170,18 @@ int main()
       cout << "" << endl;
     }
 
+    //if a game ended, ask if the users want to play again
     if(stillPlaying == false){
       cout << "Play Again? (y or n)" << endl;
       cin >> input;
+      //end game
       if(input == 'n'){
 	stillPlaying = false;
       }
+      /*
+	reset board, print out number of wins/ties, print out blank board, continue game, set the move to X,
+	and reset the number of moves
+      */
       else if (input == 'y'){
 	 for (int i = 1; i < 4; i++){
 	   for (int j = 1; j < 4; j++){
@@ -164,7 +202,6 @@ int main()
 	  playerMove = 1;
 	  moves = 0;
       }
-      
       else {
 	stillPlaying = false;
       }
@@ -172,6 +209,7 @@ int main()
   }
 }
 
+//checkWin function that checks all winning scenarios
 bool checkWin(char player, char board[4][4]){
   if (board[1][1] == player && board[1][2] == player && board[1][3] == player){
     return true;
@@ -197,6 +235,7 @@ bool checkWin(char player, char board[4][4]){
   else if (board[1][3] == player && board[2][2] == player && board[3][1] == player){
     return true;
   }
+  //none are true, so the game has not been won
   else{
     return false;
   }
